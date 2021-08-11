6f8a5c3d52a6d2b8ebf1ad02b07624fe4ab96006
fsegarra <segarrafacundoroman@gmail.com>
2021-08-11 18:57:40 -0300

    HTTP a HTTPS API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   config: {
-    urlBase: 'http://dev.physis.com.ar',
-    urlBasePhy2service: 'http://dev.physis.com.ar/phy2service',
+    urlBase: 'https://dev.physis.com.ar',
+    urlBasePhy2service: 'https://dev.physis.com.ar/phy2service',
   }
 };
